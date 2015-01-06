b269dbea8df21b01c6e92afea9d3dbd7d7c2dec3
GauravT9l <gaurav.pandey@t9l.com>
2015-01-06 11:53:41 +0530

    just Check

--- AppDelegate.m
+++ AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    // Check
+    // Check test
     return YES;
 }
 
